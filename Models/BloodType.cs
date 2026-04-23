4e30059d350421716790674bc4fe2837ef0140cf
ac146842 <ac146842@avcol.school.nz>
2026-04-23 21:56:45 +1200

    bloodtype model

---
+++ Models/BloodType.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BloodProject3.Models
+{
+    public class BloodType
+    {
+        [Key]
+        public int BloodTypeID { get; set; }
+
+        [Required]
+        [StringLength(3)]
+        public string BloodTypeName { get; set; }
+    }
+}
