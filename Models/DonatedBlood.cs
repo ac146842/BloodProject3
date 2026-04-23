bccfd5acf65659151d02e6b88dc9e1933bd7cb1d
ac146842 <ac146842@avcol.school.nz>
2026-04-23 21:57:54 +1200

    donor model

---
+++ Models/DonatedBlood.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BloodProject3.Models
+{
+    public class DonatedBlood
+    {
+        public enum Status
+        {
+            Pending = 1,
+            Quarantined = 2,
+            Approved = 3,
+            Discarded = 4,
+            Sent = 5
+        }
+
+        [Key]
+        public int DonationID { get; set; }
+
+        [ForeignKey("AppointmentID")]
+        public int AppointmentID { get; set; }
+
+        [ForeignKey("BloodTypeID")]
+        public int BloodTypeID { get; set; }
+
+        [ForeignKey("DonorID")]
+        public int DonorID { get; set; }
+
+        [Required]
+        //just the date
+        //add validation to ensure collection date is not in the future
+        public DateTime CollectionDate { get; set; }
+
+        [Required]
+        //make decimal annotation
+        public decimal VolumeML { get; set; }
+
+        [Required]
+        //just the date
+        //add validation to ensure expiry date is after collection date
+        public DateTime ExpiryDate { get; set; }
+
+        public Status BloodStatus { get; set; }
+    }
+}
---
+++ Models/Donor.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BloodProject3.Models
+{
+    public class Donor
+    {
+        [Key]
+        public int DonorID { get; set; }
+
+        [Required]
+        [ForeignKey("UserID")]
+        public int UserID { get; set; }
+
+        [Required]
+        [ForeignKey("BloodTypeID")]
+        public int BloodTypeID { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime? LastDonationDate { get; set; }
+    }
+}
