ec0a848d1ce99ccf3ba87a4beaa0f7b3e6ba2731
ac146842 <ac146842@avcol.school.nz>
2026-04-23 21:59:20 +1200

    nurse model

---
+++ Models/Nurse.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BloodProject3.Models
+{
+    public class Nurse
+    {
+        [Key]
+        public int NurseID { get; set; }
+
+        [Required]
+        [ForeignKey("UserID")]
+        public int UserID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string JobRole { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime EmployedStartDate { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string LicenseNumber { get; set; }
+    }
+}
