09ef88ee293deb71701688a16fdb10f1e027700e
ac146842 <ac146842@avcol.school.nz>
2026-04-23 21:58:58 +1200

    medicalform model

---
+++ Models/MedicalForm.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BloodProject3.Models
+{
+    public class MedicalForm
+    {
+        [Key]
+        public int FormID { get; set; }
+
+        [Required]
+        [ForeignKey("NurseID")]
+        public int NurseID { get; set; }
+
+        [Required]
+        [ForeignKey("AppointmentID")]
+        public int AppointmentID { get; set; }
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        //add validation to ensure form date is not in the future
+        public DateTime FormDate { get; set; }
+    }
+}
