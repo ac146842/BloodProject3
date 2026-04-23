0f458d43f729a93793f9358598ef3ea53d4e05f3
ac146842 <ac146842@avcol.school.nz>
2026-04-23 21:55:24 +1200

    restarting project, answers model

---
+++ Models/Answers.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BloodProject2.Models
+{
+    public class Answers
+    {
+        [Key]
+        public int AnswersID { get; set; }
+
+        [ForeignKey("HealthQID")]
+        public int HealthQID { get; set; }
+
+        [ForeignKey("AppointmentID")]
+        public int AppointmentID { get; set; }
+
+        [Required]
+        public string QuestionAnswers { get; set; }
+
+        [Required]
+        //just the date
+        //add validation to ensure answer date is not in the future
+        public DateTime AnswerDate { get; set; }
+    }
+}
