0c984f67ce967c5208f1540d69ad2825914e4da2
ac146842 <ac146842@avcol.school.nz>
2026-04-23 21:58:18 +1200

    Inventory model

---
+++ Models/Inventory.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BloodProject3.Models
+{
+    public class Inventory
+    {
+        public enum Status
+        {
+            Available = 1,
+            OnTheWay = 2,
+            Used = 3,
+            Expired = 4,
+            Reserved = 5
+        }
+
+        [Key]
+        public int BloodBankID { get; set; }
+
+        [ForeignKey("DonationID")]
+        public int DonationID { get; set; }
+
+        [ForeignKey("BloodTypeID")]
+        public int BloodTypeID { get; set; }
+
+        //make decmial annotation
+        public decimal CurrentVolumeML { get; set; }
+
+        [StringLength(15)]
+        public string StorageLocation { get; set; }
+
+        public Status BloodStatus { get; set; }
+    }
+}
