6e7e4909ba8aa518540cef3ee8e8895089d051dc
ac146842 <ac146842@avcol.school.nz>
2026-04-27 20:58:56 +1200

    appointment model updated

--- Models/Appointment.cs
+++ Models/Appointment.cs
@@ -21,33 +21,34 @@ namespace BloodProject3.Models
             PutOff = 4
         }
 
-        [Key]
+        [Key] //primary key
         public int AppointmentID { get; set; }
 
-        [Required]
+        // DonorID is required, as every appointment must be associated with a donor
         [ForeignKey("DonorID")]
         public int DonorID { get; set; }
 
-        [Required]
+        // NurseID is required, as every appointment must be associated with a nurse and done in person
+        [Required(ErrorMessage = "A nurse must be assigned.")] 
         [ForeignKey("NurseID")]
         public int NurseID { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Please select a date and time.")] //requires a date and time to be chosen and ensures date cannot be in the past with an error message
         [DataType(DataType.DateTime)]
-        //add validation to ensure appointment date and time is not in the past
+        
         public DateTime AppointmentDateTime { get; set; }
 
         [Required]
-        [StringLength(100)]
+        [StringLength(100, MinimumLength = 3)] //max location characters is 100 with a minimum of 3
+        [RegularExpression(@"^[a-zA-Z0-9\s,']+$", ErrorMessage = "Location contains invalid characters.")] //Checks for any characters that doesnt match the ones inside the brackets with an error message if invalid input is given
         public string Location { get; set; }
 
-        [Required]
-        public int AppointmentTimes { get; set; }
-
-        [Required]
+        // TypeOfAppointment is required, as every appointment must have a type with an error message if not chosen
+        [Required(ErrorMessage = "Please select the type of appointment.")]
         public AppointmentType TypeOfAppointment { get; set; }
 
-        [Required]
+        // Status is required, as every appointment must have a status with an error message if not chosen
+        [Required(ErrorMessage = "Please select a status for the appointment.")]
         public Status AppointmentStatus { get; set; }
     }
 }
