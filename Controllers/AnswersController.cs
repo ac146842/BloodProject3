2525e44171cd1bb5417e6132778e4bf0ae2b0f54
Miheer Darji <ac146842@avcol.school.nz>
2026-04-28 13:33:22 +1200

    added controllers and views back

---
+++ Controllers/AnswersController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BloodProject2.Models;
+using BloodProject3.Areas.Identity.Data;
+
+namespace BloodProject3.Controllers
+{
+    public class AnswersController : Controller
+    {
+        private readonly BloodProject3DbContext _context;
+
+        public AnswersController(BloodProject3DbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Answers
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Answers.ToListAsync());
+        }
+
+        // GET: Answers/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var answers = await _context.Answers
+                .FirstOrDefaultAsync(m => m.AnswersID == id);
+            if (answers == null)
+            {
+                return NotFound();
+            }
+
+            return View(answers);
+        }
+
+        // GET: Answers/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Answers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("AnswersID,HealthQID,AppointmentID,QuestionAnswers,AnswerDate")] Answers answers)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(answers);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(answers);
+        }
+
+        // GET: Answers/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var answers = await _context.Answers.FindAsync(id);
+            if (answers == null)
+            {
+                return NotFound();
+            }
+            return View(answers);
+        }
+
+        // POST: Answers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("AnswersID,HealthQID,AppointmentID,QuestionAnswers,AnswerDate")] Answers answers)
+        {
+            if (id != answers.AnswersID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(answers);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AnswersExists(answers.AnswersID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(answers);
+        }
+
+        // GET: Answers/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var answers = await _context.Answers
+                .FirstOrDefaultAsync(m => m.AnswersID == id);
+            if (answers == null)
+            {
+                return NotFound();
+            }
+
+            return View(answers);
+        }
+
+        // POST: Answers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var answers = await _context.Answers.FindAsync(id);
+            if (answers != null)
+            {
+                _context.Answers.Remove(answers);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AnswersExists(int id)
+        {
+            return _context.Answers.Any(e => e.AnswersID == id);
+        }
+    }
+}
---
+++ Controllers/InventoriesController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BloodProject3.Areas.Identity.Data;
+using BloodProject3.Models;
+
+namespace BloodProject3.Controllers
+{
+    public class InventoriesController : Controller
+    {
+        private readonly BloodProject3DbContext _context;
+
+        public InventoriesController(BloodProject3DbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Inventories
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Inventory.ToListAsync());
+        }
+
+        // GET: Inventories/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var inventory = await _context.Inventory
+                .FirstOrDefaultAsync(m => m.BloodBankID == id);
+            if (inventory == null)
+            {
+                return NotFound();
+            }
+
+            return View(inventory);
+        }
+
+        // GET: Inventories/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Inventories/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("BloodBankID,DonationID,BloodTypeID,CurrentVolumeML,StorageLocation,BloodStatus")] Inventory inventory)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(inventory);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(inventory);
+        }
+
+        // GET: Inventories/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var inventory = await _context.Inventory.FindAsync(id);
+            if (inventory == null)
+            {
+                return NotFound();
+            }
+            return View(inventory);
+        }
+
+        // POST: Inventories/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("BloodBankID,DonationID,BloodTypeID,CurrentVolumeML,StorageLocation,BloodStatus")] Inventory inventory)
+        {
+            if (id != inventory.BloodBankID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(inventory);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!InventoryExists(inventory.BloodBankID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(inventory);
+        }
+
+        // GET: Inventories/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var inventory = await _context.Inventory
+                .FirstOrDefaultAsync(m => m.BloodBankID == id);
+            if (inventory == null)
+            {
+                return NotFound();
+            }
+
+            return View(inventory);
+        }
+
+        // POST: Inventories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var inventory = await _context.Inventory.FindAsync(id);
+            if (inventory != null)
+            {
+                _context.Inventory.Remove(inventory);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool InventoryExists(int id)
+        {
+            return _context.Inventory.Any(e => e.BloodBankID == id);
+        }
+    }
+}
